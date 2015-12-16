e23fb3be7bc8964019ad32217dd7d7251fa68577
Your Name <you@example.com>
2015-12-16 19:54:20 +0000

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 apt-get update && apt-get install -y git
-git clone https://github.com/reactioncommerce/reaction.git
-cd reaction
-./reaction pull
-git clone https://github.com/elkridgelodge/sip-workflow.git
-cd sip-workflow
-mv sip-workflow-package packages
+git clone https://github.com/reactioncommerce/reaction.git $HOME/reaction
+$HOME/reaction/reaction pull
+git clone https://github.com/elkridgelodge/sip-workflow.git $HOME/reaction/sip-workflow
+mv $HOME/reaction/sip-workflow/sip-workflow-package $HOME/reaction/packages
 meteor add sip-workflow-package
-cp private/data/Shops.json ../../packages/reaction-sample-data/private/data
+cp $HOME/reaction/sip-workflow/private/data/Shops.json $HOME/reaction/packages/reaction-sample-data/private/data
+$HOME/reaction/reaction
