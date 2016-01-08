0ef0b5e657be07a4c00468cc77f81cb252ecc1b1
Your Name <you@example.com>
2016-01-08 18:55:07 +0000

    install change

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get update && apt-get install -y git
 git clone https://github.com/reactioncommerce/reaction.git $HOME/reaction
 $HOME/reaction/reaction pull
 git clone https://github.com/elkridgelodge/sip-workflow.git $HOME/reaction/sip-workflow
-mv $HOME/reaction/sip-workflow/sip-workflow-package $HOME/reaction/packages
+cp -r $HOME/reaction/sip-workflow/sip-workflow-package $HOME/reaction/packages
 #cp $HOME/reaction/sip-workflow/private/data/Shops.json $HOME/reaction/packages/reaction-sample-data/private/data
 echo "cd reaction"
 echo "meteor add sip-workflow-package"
