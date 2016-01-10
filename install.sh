104c1520a6c8bd07cab383e1e56176a30fb1e740
Your Name <you@example.com>
2016-01-10 21:09:32 +0000

    auto-installing package

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ apt-get update && apt-get install -y git
 git clone https://github.com/reactioncommerce/reaction.git $HOME/reaction
 $HOME/reaction/reaction pull
 git clone https://github.com/elkridgelodge/sip-workflow.git $HOME/reaction/packages/sip-workflow
+meteor add sip-workflow
 echo "cd reaction"
-echo "meteor add sip-workflow"
 echo "./reaction"
