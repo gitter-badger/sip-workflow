289c6250aefa8866fccf4bc974332f0d301f1276
Your Name <you@example.com>
2016-01-10 21:29:41 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@ apt-get update && apt-get install -y git
 git clone https://github.com/reactioncommerce/reaction.git $HOME/reaction
 $HOME/reaction/reaction pull
 git clone https://github.com/elkridgelodge/sip-workflow.git $HOME/reaction/packages/sip-workflow
+echo ""
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@@ Everything install OK? @@@"
-echo "@@@ If so, next steps are:"
+echo "@@@ If so, next steps are: @@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo ""
 echo "cd reaction"
 echo "meteor add sip-workflow"
